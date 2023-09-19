e9811f1ea1272f4ee38ea17ff3e7a46a96c3d25a
David Lgc <legouellecd@gmail.com>
2023-09-19 12:45:00 +0200

    Fix controllers + class import

--- src/Form/WifiParamsType.php
+++ src/Form/WifiParamsType.php
@@ -3,31 +3,33 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class WifiParamsType extends AbstractType
 {
-  public function buildForm(FormBuilderInterface $builder, array $options): void
-  {
-    $builder
-      ->add('networkName', TextType::class)
-      ->add('encryption', ChoiceType::class, [
-        'choices' => [
-            'WPA2' => 'WPA2',
-            'WPA' => 'WPA',
-            'WEP' => 'WEP',
-        ],
-        'help' => 'The type of security protocol on your network.'
-      ])
-        ->add('password', TextType::class)
-        ->add('isHidden', CheckboxType::class, [
-          'label' => 'Hidden Wifi Network',
-          'required' => false
-        ])
-    ;
-  }
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('networkName', TextType::class)
+            ->add('encryption', ChoiceType::class, [
+                'choices' => [
+                    'WPA2' => 'WPA2',
+                    'WPA' => 'WPA',
+                    'WEP' => 'WEP',
+                ],
+                'help' => 'The type of security protocol on your network.',
+            ])
+            ->add('password', TextType::class)
+            ->add('isHidden', CheckboxType::class, [
+                'label' => 'Hidden Wifi Network',
+                'required' => false,
+            ])
+        ;
+    }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
